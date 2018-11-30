f0de625e24c9edab009e324ce8d18346a561f36f
wstmarc <wilfried.saintmarc@gmail.com>
2018-11-30 15:48:44 +0100

    Ajout d'un commentaire de test

--- src/main/java/fr/laerce/cinema2/service/Affiche.java
+++ src/main/java/fr/laerce/cinema2/service/Affiche.java
@@ -1,9 +1,5 @@
-package fr.laerce.cinema2.web;
+package fr.laerce.cinema2.service;
 
-import fr.laerce.cinema2.dao.FilmsDAO;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
@@ -14,38 +10,11 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.OutputStream;
 
-@Controller
-public class MainController {
-
-    @Autowired
-    FilmsDAO filmsDAO;
-
-    @GetMapping("/")
-    public String main(Model model){
-        model.addAttribute("nom","Karl");
-        model.addAttribute("films",filmsDAO.films());
-        return "index";
-    }
-
-    @GetMapping("/film/{id}")
-    public String detail(Model model, @PathVariable("id") String id){
-        Integer idFilm = Integer.parseInt(id);
-        model.addAttribute("film", filmsDAO.getById(idFilm));
-        return "detail";
-    }
-
-//TODO Essayer d'externaliser le controlleur
-/*    @GetMapping("affiche/{id}")
-    public String affiche(Model model, @PathVariable("id") String id){
-        Integer idFilm = Integer.parseInt(id);
-        String nomimagefilm = filmsDAO.getById(idFilm).getAfficheNom();
-        model.addAttribute("nomfilm", nomimagefilm);
-        return "affiche";
-    }*/
-    //on créer une methode affiche qui est mapper /affiche/id avec id, le nom de l'affiche du film
+public class Affiche {
+        //on créer une methode affiche qui est mapper /affiche/id avec id, le nom de l'affiche du film
     @GetMapping("/affiche/{id}")
     public void affiche (HttpServletRequest request, HttpServletResponse response, @PathVariable("id") String id) throws IOException {
-        
+
 //merci patrick
         /////////////////////////////////////////////////////
         //on copie colle le code du prof et on adapte
